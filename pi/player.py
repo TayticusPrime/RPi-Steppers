ba4dfeda2c32934ea22f00f191f6e3c398f5aed3
Tayte Waterman <tayte.d.waterman@gmail.com>
2024-12-19 17:45:17 +0100

    Add root-level songs directory and runner script

--- pi/player.py
+++ pi/player.py
@@ -1,3 +1,14 @@
+#player.py
+
+'''
+Author: Tayte Waterman
+Date: Dec 2024
+About: The following script reads external MIDI files and translates up to 8 channels
+of the contained content into serial commands to play via Arduino-controlled
+stepper motors.
+'''
+
+#Dependencies
 import argparse
 import serial
 import struct
@@ -11,6 +22,8 @@ INIT_TIME = 2.0
 STEPPERS_PER_CONTROLLER = 4 
 MAX_CONTROLLERS = 2
 QUANTA = 0.05
+
+DEFAULT_KEYSHIFT = -12
 
 #RequestMode Enum
 OFF = 0
@@ -43,11 +56,17 @@ class Track:
     def restart(self):
         self.index = 1
     
-    def update(self, timestamp):
+    def update(self, timestamp, keyshift=DEFAULT_KEYSHIFT):
         if self.index < len(self.track.notes):
             if timestamp >= self.track.notes[self.index].start:
                 note = self.track.notes[self.index]
-                self.arduino.send(self.stepper, PULSE, NOTES[note.pitch - 12], note.end - note.start)
+                pitch = note.pitch + keyshift
+                if pitch >= NOTES['MIN'] and pitch <= NOTES['MAX']:
+                    self.arduino.send(self.stepper,
+                                      PULSE,
+                                      NOTES[pitch],
+                                      note.end - note.start
+                                      )
                 self.index += 1
             return False
 
@@ -73,7 +92,7 @@ class Player:
             j = index % STEPPERS_PER_CONTROLLER
             self.tracks.append(Track(instrument, self.controllers[i], j+1))
 
-    def run(self, loop=False):
+    def run(self, loop=False, keyshift=DEFAULT_KEYSHIFT):
         print(f'Playing file \"{self.filename}\"...')
 
         while(True):
@@ -86,7 +105,7 @@ class Player:
                 timestamp = time.time() - start
                 complete = True
                 for track in self.tracks:
-                    status = track.update(timestamp)
+                    status = track.update(timestamp, keyshift)
                     complete = complete and status
                 time.sleep(QUANTA)
 
@@ -99,11 +118,11 @@ def main():
 
     parser.add_argument('filename', help='Input MIDI file of type .mid')
     parser.add_argument('--loop', action='store_true', help='Continuously replay file')
-
+    parser.add_argument('-k', type=int, help='Number of notes to keyshift the original MIDI file by. Default=-12', default=DEFAULT_KEYSHIFT)
     args = parser.parse_args()
     
     player = Player(args.filename)
-    player.run(args.loop)
+    player.run(loop=args.loop, keyshift=args.k)
 
 if __name__ == "__main__":
     main()
